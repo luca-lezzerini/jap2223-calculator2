8ea25cf8e7d04ef268047f340067c6dbcd0ff667
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-11-18 08:14:37 +0100

    First commit with empty springboot project template

---
+++ src/test/java/al/edu/universitetipolis/calculator2/Calculator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package al.edu.universitetipolis.calculator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
